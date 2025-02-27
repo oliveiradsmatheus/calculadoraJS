4f8dde1047bd614beb370d568c6074e6358918c4
oliveiradsmatheus <oliveiradsmatheus@gmail.com>
2025-02-27 20:39:05 -0300

    Projeto finalizado

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,8 +1,64 @@
-const trocarTema = document.getElementById('trocarTema');
-const calculadora = document.getElementById('calculadora');
-let escuro = true;
+let calculado = false;
+const op = "";
 
-trocarTema.onclick = () => {
-    calculadora.classList.toggle('escuro');
-    trocarTema.classList.toggle('ativo');
-};
+function inserir(num) {
+    var numero = document.getElementById('visor').innerHTML;
+
+    if (num >= '0' && num <= '9') {
+        if (calculado) {
+            document.getElementById('visor').innerHTML = num;
+            calculado = false;
+        }
+        else
+            if (numero === '0')
+                document.getElementById('visor').innerHTML = num;
+            else
+                document.getElementById('visor').innerHTML = numero + num;
+    } else {
+        if (calculado) {
+            document.getElementById('visor').innerHTML = numero + num;
+            calculado = false;
+        } else {
+            if (numero[numero.length - 1] >= 0 && numero[numero.length - 1] <= 9)
+                document.getElementById('visor').innerHTML = numero + num;
+            else {
+                numero = numero.slice(0, numero.length - 1);
+                document.getElementById('visor').innerHTML = numero + num;
+            }
+        }
+    }
+}
+
+function apagar() {
+    numero = document.getElementById('visor').innerHTML;
+    if (calculado)
+        numero = '0';
+    else {
+        if (numero[numero.length - 2] === '.')
+            numero = numero.slice(0, numero.length - 2);
+        else
+            numero = numero.slice(0, numero.length - 1);
+        if (numero.length === 0)
+            numero = '0';
+    }
+    document.getElementById('visor').innerHTML = numero;
+}
+
+function limpar() {
+    document.getElementById('visor').innerHTML = "0";
+}
+
+function calcular() {
+    var resultado = document.getElementById('visor').innerHTML;
+    resultado = resultado.replace("÷", "/");
+    resultado = resultado.replace('×', '*');
+    if (resultado) {
+        resultado = eval(resultado);
+        if (resultado % 1 !== 0)
+            resultado = resultado.toFixed(5);
+        document.getElementById('visor').innerHTML = eval(resultado);
+        calculado = true;
+    }
+    else
+        document.getElementById('visor').innerHTML = "Erro";
+}
---
+++ scripts/tema.js
@@ -0,0 +1,29 @@
+function trocarTema() {
+    var body = document.querySelector('body');
+    var root = document.querySelector(':root');
+    var troca = document.getElementById('trocarTema');
+
+    troca.classList.toggle('ativo');
+
+    if (body.dataset.theme === 'escuro') {
+        root.style.setProperty('--fundo', '#d7d7d7');
+        root.style.setProperty('--fonte', '#282A36');
+        root.style.setProperty('--botao', '#F8F8F2');
+        root.style.setProperty('--visor', 'rgba(248, 248, 242, 0.65)');
+        root.style.setProperty('--calculadora', 'rgba(200, 200, 200, 0.65)');
+        root.style.setProperty('--sombra', '#44475A')
+        root.style.setProperty('--operador', '#F1FA8C');
+        root.style.setProperty('--digito', '#282A36');
+        body.dataset.theme = 'claro';
+    } else {
+        root.style.setProperty('--fundo', '#353740');
+        root.style.setProperty('--fonte', '#FFB86C');
+        root.style.setProperty('--botao', '#282A36');
+        root.style.setProperty('--visor', 'rgba(40, 42, 54, 0.65)');
+        root.style.setProperty('--calculadora', 'rgba(67, 69, 82, 0.65)');
+        root.style.setProperty('--sombra', '#FFB86C')
+        root.style.setProperty('--operador', '#BD93F9');
+        root.style.setProperty('--digito', '#F8F8F2');
+        body.dataset.theme = 'escuro';
+    }
+}
